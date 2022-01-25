b433a65c32df03aaf679e9670b666ad0b68b2a37
Nafis Handoko <naffisioner@gmail.com>
2022-01-25 17:18:34 +0700

    add basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,38 @@ function App() {
 
   return (
     <div className="container">
+      <header>
+        <h1>25 + 5 Clock</h1>
+      </header>
+      <section>
+        <div className='break-session'>
+          <div className="break">
+            <div id="break-label">Break Length</div>
+            <div className="break-control">
+              <button id="break-decrement">-</button>
+              <span id="break-length">5</span>
+              <button id="break-increment">+</button>
+            </div>
+          </div>
+          <div className="session">
+          <div id="session-label">Session Length</div>
+            <div className="session-control">
+              <button id="session-decrement">-</button>
+              <span id="session-length">5</span>
+              <button id="session-increment">+</button>
+            </div>
+          </div>
+        </div>
+        <div className='clock'>
+          <div id="timer-label">Session</div>
+          <div id="time-left">25:00</div>
+          <div className="time-control">
+            <button id="start_stop"></button>
+            <button id="reset"></button>
+          </div>
+        </div>
+      </section>
+      <footer></footer>
     </div>
   )
 }
